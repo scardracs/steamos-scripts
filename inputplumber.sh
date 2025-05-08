eccb6cda34f8e8876e7c225ad055e55e1e5c2248
Marco Scardovi <mscardovi95@gmail.com>
2025-05-08 19:51:17 +0200

    Update inputplumber.sh

--- inputplumber.sh
+++ inputplumber.sh
@@ -1,3 +1,12 @@
-echo "This script will use sudo. Be sure to have set a password with passwd before run it. Press enter when you are ok."
-pause
-sudo systemctl enable --now inputplumber{,-suspend}
+echo "Do you have set a password? Type YES or NO"
+read shell
+if [$shell = NO] || [$shell = no]
+	passwd
+  echo "This script will use sudo."
+  sudo systemctl enable --now inputplumber{,-suspend}
+elif[$shell = YES] || [$shell = yes]
+  echo "This script will use sudo."
+  sudo systemctl enable --now inputplumber{,-suspend}
+else
+  echo "What tf did you tipe? Please use NO|no or YES|yes"
+fi
